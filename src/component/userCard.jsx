e0b698e681e272f4e0a1be48f7799ed220f6513a
snehil <snehil@snehils-MacBook-Air.local>
2025-06-25 00:35:26 +0530

    feed api integration

---
+++ src/component/userCard.jsx
@@ -0,0 +1,18 @@
+function UserCard({ user }) {
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img src={user?.photoUrl} alt="Photo" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{`${user.firstName} ${user.lastName}`}</h2>
+        <p>{user.about}</p>
+        <div className="card-actions justify-center my-4">
+          <button className="btn btn-primary">Ignore</button>
+          <button className="btn btn-secondary">Interested</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default UserCard;
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import UserCard from "../component/userCard";
+import { BASE_URL } from "../utils/constant";
+import { addFeed } from "../utils/feedSlice";
 
-const Feed = () => {
-  return <div>Feed</div>;
-};
-
+function Feed() {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+  async function getFeed() {
+    try {
+      const res = await axios.get(`${BASE_URL}/user/feed`, {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data?.data));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    getFeed();
+  }, []);
+  console.log("feed", feed);
+  if (Array.isArray(feed) && feed.length === 0) return null;
+  return (
+    <div className="flex justify-center my-10">
+      {feed?.length &&
+        feed.map((specificUser) => {
+          return <UserCard user={specificUser} key={specificUser._id} />;
+        })}
+    </div>
+  );
+}
 export default Feed;
