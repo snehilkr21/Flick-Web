6c8c5e54e4908ba4c280cc1dff46d9315bc9eaec
Snehil <snehil@192.168.1.6>
2026-06-26 21:26:55 +0530

    socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,12 +1,10 @@
-import { io } from "socket.io-client";
+import io from "socket.io-client";
+import { BASE_URL } from "../utils/constant";
 
 export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
-    return io("http://localhost:7777");
+    return io(BASE_URL);
+  } else {
+    return io("/", { path: "/api/socket.io" });
   }
-
-  return io({
-    path: "/socket.io",
-    withCredentials: true,
-  });
 };
